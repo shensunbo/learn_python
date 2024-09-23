1a86291cea7f2ccf6f9b31f5219d9a84454157fc
shensunbo <shensunbo@foxmail.com>
2024-09-23 21:12:21 +0800

    class test

--- day1/classTest.py
+++ day1/classTest.py
@@ -1,6 +1,35 @@
-
+class Test:
+    s = "hello"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def show(self):
+        print("name:", self.name, " age:", self.age)
+        
+    @staticmethod
+    def retS():
+        return Test.s
 
 #-------------------------------------------------
 
 if __name__ == "__main__":
     print("class test")
+    
+    classTest = Test("bob", 123)
+    classTest.show()
+    classTest.name = "linda"
+    classTest.age = 9
+    classTest.show()
+    
+    classTest.gender = True;
+    print(classTest.gender)
+    print(Test.s)
+    Test.s = "helloworld"
+    print(Test.s)
+    print(getattr(classTest, "gender"))
+    
+    print(Test.__name__)
+    print(Test.__doc__)
+    
+    print(Test.retS())
+    
---
+++ day1/classTest2.py
@@ -0,0 +1,28 @@
+class Base:
+    attr = "hello"
+    def __init__(self, name, age):
+        print("Base init")
+        self.name = name
+        self.age = age
+    def show(self):
+        print("base name:", self.name, " age:", self.age)
+
+class subClass(Base):
+    def __init__(self, name, age):
+        print("subClass init")
+        Base.__init__(self, name, age)
+        
+    def show(self):
+        print("sub name:", self.name, " age:", self.age)
+        Base.show(self)
+        
+        
+#-------------------------------------------------
+
+if __name__ == "__main__":
+    sub = subClass("bob", 123)
+    sub.show()
+    print(sub.attr)
+    sub.age = 100
+    sub.name = "linda"
+    sub.show()
